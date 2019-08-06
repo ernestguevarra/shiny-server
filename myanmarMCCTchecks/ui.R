c1f772ca7d9e1d09645477e5dad70736898895d3
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-06 10:37:26 +0100

    update ui.R

--- myanmarMCCTchecks/ui.R
+++ myanmarMCCTchecks/ui.R
@@ -58,8 +58,25 @@ ui <- dashboardPage(
   dashboardBody(
     ## Specify a custom.css
     tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "valid.css")
+      tags$link(rel = "stylesheet", type = "text/css", href = "valid.css"),
+      HTML(
+        "
+        <script>
+        var socket_timeout_interval
+        var n = 0
+        $(document).on('shiny:connected', function(event) {
+        socket_timeout_interval = setInterval(function)(){
+        Shiny.onInputChange('count', n++)
+        }, 15000)
+        });
+        $(document).on('shiny:disconnected', function(event) {
+        clearInterval(socket_timeout_interval)
+        });
+        </script>
+        "
+      )
     ),
+    textOutput("keepAlive"),
     ## Body outputs for every menu item on sidebar
     tabItems(
       ## Body output when 'parameters' menu is selected
